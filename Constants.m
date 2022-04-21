8ae5697e6247711feebdc6060dedd648f43c02e3
and03170 <and03170@umn.edu>
2022-04-21 16:28:15 -0500

    Added initial Constants file

---
+++ Constants.m
@@ -0,0 +1,21 @@
+Sun.mu = 132712440018; % km^3/s^2
+Earth.mu = 398600;
+Venus.mu = 324859;
+Jupiter.mu = 126686534;
+
+Earth.radius = 6378; % km
+Venus.radius = 6052;
+Jupiter.radius = 71490;
+
+Earth.minPeriapsis = Earth.radius + 300; % km
+Venus.minPeriapsis = Venus.radius + 16000;
+
+
+
+
+
+
+
+
+
+
