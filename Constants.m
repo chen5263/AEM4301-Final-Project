476fcf9b05d8e41204d30947d5d422635939bb17
and03170 <and03170@umn.edu>
2022-04-21 17:11:31 -0500

    Added more stuff to Constants

--- Constants.m
+++ Constants.m
@@ -10,11 +10,30 @@ Jupiter.radius = 71490;
 Earth.minPeriapsis = Earth.radius + 300; % km
 Venus.minPeriapsis = Venus.radius + 16000;
 
+% Planet ID#s
+ID.Mercury =1;
+ID.Venus =2;
+ID.Earth =3;
+ID.Mars =4;
+ID.Jupiter =5;
+ID.Saturn = 6;
+ID.Uranus =7;
+ID.Neptune =8;
+ID.Pluto =9;
 
+Day1.year = 1989;
+Day1.month = 10;
+Day1.day = 18;
+Day1.hour = 0;
+Day1.minute = 0;
+Day1.second = 0;
 
-
-
-
+Deadline.year = 1995;
+Deadline.month = 12;
+Deadline.day = 7;
+Deadline.hour = 0;
+Deadline.minute = 0;
+Deadline.second = 0;
 
 
 
